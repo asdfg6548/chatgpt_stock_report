0571c709fefb9958b61d0d98ed426bf660e2f6d0
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-05-28 22:35:51 +0900

    good

---
+++ utils.py
@@ -0,0 +1,23 @@
+import time
+import openai
+from openai import OpenAI
+
+
+def make_requests(engine, prompts,api_key=None):
+    client = OpenAI(api_key= api_key)
+    retries = 3
+    retry_cnt = 0
+    backoff_time = 10
+    while retry_cnt <= retries:
+        try:
+            response = client.chat.completions.create(
+                model=engine,
+                messages = [{"role": "user","content":prompts}]
+            )
+            break
+        except:
+            print(f"Retrying in {backoff_time} seconds...")
+            time.sleep(backoff_time)
+            backoff_time *= 1.5
+            retry_cnt += 1
+    return response
